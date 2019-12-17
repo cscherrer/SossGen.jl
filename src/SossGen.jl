bc01be09da0a5a839657645c113f5798096b0ecd
Chad Scherrer <chad.scherrer@gmail.com>
2019-12-17 11:53:13 -0800

    Reorganize to follow https://probcomp.github.io/Gen/dev/ref/extending/#Custom-generative-functions-1

--- src/SossGen.jl
+++ src/SossGen.jl
@@ -12,13 +12,14 @@ import Gen
 include("utils.jl")
 
 
-NTtypes(NT::Type{NamedTuple{N,T}}) where {N,T} = namedtuple(getN(NT),getT(NT))
+# Following https://probcomp.github.io/Gen/dev/ref/extending/#Custom-generative-functions-1
+# Define a trace data type
 
 struct SossTrace{F, NT} <: Gen.Trace
-  gen_fn :: F
-  choices :: NT
-  logprob :: Float64
-  args
+    gen_fn :: F
+    choices :: NT
+    logprob :: Float64
+    args
 end
 
 # Define a data type for your generative function
@@ -28,6 +29,8 @@ struct SossGenerativeFunction{M,F,V}  <: Gen.GenerativeFunction{V,SossTrace{Soss
     model  :: M
     logpdf :: F
 end
+
+# Decide what the arguments to a generative function should be
 
 function SossGenerativeFunction(m::Model{A,B,M}, ::typeof(codegen)) where {A,B,M}
     ℓ(args, data) = logpdf(m(args...), data, codegen)::Float64
@@ -39,43 +42,66 @@ function SossGenerativeFunction(m::Model{A,B,M}) where {A,B,M}
     SossGenerativeFunction{Model{A,B,M},typeof(ℓ), typeof(variables(m))}(m, ℓ)
 end
 
+# Implement methods of the Generative Function Interface
+
+# simulate
+
 function Gen.simulate(gen_fn::SossGenerativeFunction{M,F,V}, args::Tuple) where {M,F,V}
     choices = rand(gen_fn.model(args...))
     score = gen_fn.logpdf(args, choices)
     SossTrace{SossGenerativeFunction,NT}(gen_fn, choices, score, args)
 end
 
-function Gen.generate(gen_fn::SossGenerativeFunction{M,F,V}, args::Tuple, constraints::Gen.ChoiceMap) where {M,F,V}
-  kvs = Gen.get_values_shallow(constraints)
-  data = namedtuple(Dict{Symbol, Any}(kvs))
-  weight, choices = weightedSample(gen_fn.model(args...), data)
-  logprob = gen_fn.logpdf(args, choices)
-  SossTrace{SossGenerativeFunction,typeof(choices)}(gen_fn, choices, logprob, args), weight
+# has_argument_grads
+
+# accepts_output_grad
+
+# get_args
+
+function Gen.get_args(t::SossTrace{SossGenerativeFunction,NT}) where {NT}
+    t.args
 end
+
+# get_retval
+
+function Gen.get_retval(t::SossTrace{SossGenerativeFunction,NT}) where {NT}
+    t.choices
+end
+
+# get_choices
+
+function Gen.get_choices(t::SossTrace{SossGenerativeFunction,NT}) where {NT}
+    c = Gen.choicemap()
+    Ts = NTtypes(NT)
+    for (k::Symbol, v) in pairs(t.choices)
+        c[k] = v :: Ts[k]
+    end
+    c
+end
+
+# get_score
+
+function Gen.get_score(t::SossTrace{SossGenerativeFunction,NT}) where {NT}
+    t.logprob
+end
+
+# get_gen_fn
+
+# project
 
 Gen.project(t::SossTrace{SossGenerativeFunction,NT}, ::Gen.EmptySelection) where {NT} = 0.0
 
-function Gen.get_retval(t::SossTrace{SossGenerativeFunction,NT}) where {NT}
-  t.choices
+# generate
+
+function Gen.generate(gen_fn::SossGenerativeFunction{M,F,V}, args::Tuple, constraints::Gen.ChoiceMap) where {M,F,V}
+    kvs = Gen.get_values_shallow(constraints)
+    data = namedtuple(Dict{Symbol, Any}(kvs))
+    weight, choices = weightedSample(gen_fn.model(args...), data)
+    logprob = gen_fn.logpdf(args, choices)
+    SossTrace{SossGenerativeFunction,typeof(choices)}(gen_fn, choices, logprob, args), weight
 end
 
-function Gen.get_args(t::SossTrace{SossGenerativeFunction,NT}) where {NT}
-  t.args
-end
-
-function Gen.get_score(t::SossTrace{SossGenerativeFunction,NT}) where {NT}
-  t.logprob
-end
-
-
-function Gen.get_choices(t::SossTrace{SossGenerativeFunction,NT}) where {NT}
-  c = Gen.choicemap()
-  Ts = NTtypes(NT)
-  for (k::Symbol, v) in pairs(t.choices)
-    c[k] = v :: Ts[k]
-  end
-  c
-end
+# update
 
 function Gen.update(t::SossTrace{SossGenerativeFunction,NT}, new_args::Tuple, argdiffs::Tuple, constraints::Gen.ChoiceMap) where {NT}
     retdiff = ifelse(isempty(constraints), Gen.NoChange(), Gen.UnknownChange())
@@ -85,9 +111,15 @@ function Gen.update(t::SossTrace{SossGenerativeFunction,NT}, new_args::Tuple, ar
     weight = new_logprob - t.logprob
     discard = Gen.choicemap()
     for (k::Symbol, v) in Gen.get_values_shallow(constraints)
-      discard[k] = t.choices[k]
+        discard[k] = t.choices[k]
     end
     return (new_trace, weight, retdiff, discard)
 end
+
+# regenerate
+
+# choice_gradients
+
+# accumulate_param_gradients!
 
 end 
